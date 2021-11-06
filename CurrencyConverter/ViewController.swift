745f53731afc81b4727026b81276b47a6ffaac16
BUKET UYSAL KUYU <>
2021-11-06 16:39:26 +0000

    Add User Interfaces

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by BUKET UYSAL KUYU on 6.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblUsd: UILabel!
+    @IBOutlet weak var lblGbp: UILabel!
+    @IBOutlet weak var lblCad: UILabel!
+    @IBOutlet weak var lblTry: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func getRatesClicked(_ sender: Any) {
+    }
+    
+}
+
